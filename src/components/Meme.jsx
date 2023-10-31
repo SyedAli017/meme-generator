228265881f4fa5eb9e72460de144a539bb52dd51
Syed Ali <sydali3103@gmail.com>
2023-10-31 08:14:35 +0530

    changed from to div

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,13 +3,13 @@ import "../styles/meme.css";
 const Meme = () => {
   return (
     <div>
-      <form className="form">
+      <div className="form">
         <div className="input-container">
           <input className="form-input" type="text" placeholder="Top text" />
           <input className="form-input" type="text" placeholder="Bottom text" />
         </div>
         <button className="meme-button">Get a new meme image 🖼</button>
-      </form>
+      </div>
     </div>
   );
 };
